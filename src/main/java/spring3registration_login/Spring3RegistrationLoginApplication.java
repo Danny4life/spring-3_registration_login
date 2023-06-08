e72666ae9f5a5ac5a6091ac31f95c1923ce00796
Segun Osiki <osiki.segun@gmail.com>
2023-06-08 08:42:57 +0100

    commit initial file

---
+++ src/main/java/spring3registration_login/Spring3RegistrationLoginApplication.java
@@ -0,0 +1,13 @@
+package spring3registration_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring3RegistrationLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring3RegistrationLoginApplication.class, args);
+	}
+
+}
